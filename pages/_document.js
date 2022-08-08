5312e01b25f2931575c118514c99d9b49a3bb18a
fyb <fyb@paiyou.co>
2022-08-08 10:02:31 +0800

    fix: css

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, { Children } from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import createEmotionServer from '@emotion/server/create-instance'
 import createEmotionCache from '../utils/createEmotionCache'
@@ -36,7 +36,7 @@ CustomDocument.getInitialProps = async (ctx) => {
 
   return {
     ...initialProps,
-    emotionStyleTags,
+    styles: [...Children.toArray(initialProps.styles), ...emotionStyleTags],
   }
 }
 
